d119fa56678ac2c782275d04baef59d276c53b7e
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-22 17:58:39 +0700

    memodifikasi file SiakadWhile11.java

--- SiakadWhile11.java
+++ SiakadWhile11.java
@@ -12,29 +12,33 @@ public class SiakadWhile11 {
             
         
         while (i < jumlahMahasiswa) {
-            System.out.print("Masukkan nilai mahasiswa ke-: ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             int nilai = scanner.nextInt();
 
             if (nilai < 0 || nilai > 100) {
                 System.out.println("Nilai tidak valid. Masukkan nilai antara 0 hingga 100.");
                 continue;
             }
+
+            // i++;
             if (nilai > 80 && nilai <= 100) {
-                System.out.println(i + "Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println(i + ". " + "Nilai mahasiswa ke-" + (i + 1) + " adalah A");
             } else if (nilai > 73 && nilai <= 80) {
-                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+                System.out.println(i + ". " + "Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
-                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+                System.out.println(i + ". " + "Nilai mahasiswa ke-" + (i + 1) + " adalah B");
             } else if (nilai > 60 && nilai <= 65) {
-                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+                System.out.println(i + ". " + "Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
             } else if (nilai > 50 && nilai <= 60) {
-                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+                System.out.println(i + ". " + "Nilai mahasiswa ke-" + (i + 1) + " adalah C");
             } else if (nilai > 39 && nilai <= 50) {
-                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+                System.out.println(i + ". " + "Nilai mahasiswa ke-" + (i + 1) + " adalah D");
             } else {
-                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+                System.out.println(i + ". " + "Nilai mahasiswa ke-" + (i + 1) + " adalah E");
             }
             i++;
         }
+        
+        scanner.close();
     }
 }
