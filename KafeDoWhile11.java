c15b647ceace38bd3c57bd50771683563fa7e069
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-23 08:21:41 +0700

    mengisi file kafe dowhile.java

--- KafeDoWhile11.java
+++ KafeDoWhile11.java
@@ -1,6 +1,25 @@
 public class KafeDoWhile11 {
     public static void main(String[] args) {
-        
-       System.out.println("Percobaan 3: Menggunakan while untuk menghitung jumlah karakter vokal dalam sebuah string");
+        int i = 0;
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Masukkan jumlah kopi yang ingin dipesan: ");
+        int jumlahKopi = scanner.nextInt();
+
+        do {
+            System.out.print("Masukkan jenis kopi ke-" + (i + 1) + ": ");
+            String jenisKopi = scanner.next();
+
+            System.out.print("Masukkan ukuran kopi ke-" + (i + 1) + " (S/M/L): ");
+            String ukuranKopi = scanner.next();
+
+            System.out.print("Masukkan jumlah gula untuk kopi ke-" + (i + 1) + ": ");
+            int jumlahGula = scanner.nextInt();
+
+            System.out.println("Pesanan kopi ke-" + (i + 1) + ": " + jenisKopi + ", Ukuran: " + ukuranKopi + ", Gula: " + jumlahGula + " sendok");
+            i++;
+        } while (i < jumlahKopi);
+
+        scanner.close();
     }
 }
